80b006fc36e53bad54dfd048280ed70fbdaadd2b
serginho6x <serginho_6x@hotmail.com>
2020-04-29 15:18:33 -0400

    Guarda y carga llamadas

---
+++ src/Archivo.java
@@ -0,0 +1,56 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+
+public class Archivo {
+	
+	public void guardarLlamada(Llamada llamada) {
+		try {
+			File f = new File("llamadas.txt");
+			FileWriter fw;
+			BufferedWriter bw;
+			if(f.exists()){
+				fw = new FileWriter(f,true);
+				bw = new BufferedWriter(fw);
+				bw.newLine();
+				bw.write(llamada.getNumeroLlamante() + "%" + llamada.getNumeroLlamado());
+			}else {
+				fw = new FileWriter(f);
+				bw = new BufferedWriter(fw);
+				bw.write(llamada.getNumeroLlamante() + "%" + llamada.getNumeroLlamado());
+			}
+			bw.close();
+			fw.close(); 
+		} catch (Exception e) {
+			System.out.println(e);
+		}
+	}
+	
+	public void imprimirLlamadas() {
+		try {
+			File f = new File("llamadas.txt");
+			if(f.exists()) {
+				FileReader fr = new FileReader(f);
+				BufferedReader br = new BufferedReader(fr);
+				String linea;
+				while((linea = br.readLine()) != null) {
+					String [] contacto = linea.split("%");
+					Llamada p = new Llamada();
+					p.setNumeroLlamante(contacto[0]);
+					p.setNumeroLlamado(contacto[1]);
+					System.out.println(p.getNumeroLlamante());
+					System.out.println(p.getNumeroLlamado());
+					System.out.println("******************");
+				}
+				br.close();
+			}
+			
+		} catch (Exception e) {
+			System.out.println(e);
+		}
+	}
+	
+	
+}
--- src/main.java
+++ src/main.java
@@ -1,24 +1,62 @@
+import java.util.ArrayList;
+import java.util.List;
 
 public class main {
 
-	public static void pruebasDeGuardaryCargarLlamadas() {
-		
-		FicherosBinarios ficherosBinarios = new FicherosBinarios();
+	public static void pruebaDeGuardadVariasLlamadas() {
+		Archivo archivo = new Archivo();
 		Llamada llamada1 = new Llamada();
-		String numeroDeHenry="12345678";
-		String numeroDeSergio="0000";
+		String numeroDeHenry="00000";
+		String numeroDeSergio="777777";
+		llamada1.setNumeroLlamante(numeroDeHenry);
+		llamada1.setNumeroLlamado(numeroDeSergio);
+		Llamada llamada2 = new Llamada();
+		numeroDeHenry="33333333";
+		numeroDeSergio="4444444";
+		llamada2.setNumeroLlamante(numeroDeHenry);
+		llamada2.setNumeroLlamado(numeroDeSergio);
+		Llamada llamada3 = new Llamada();
+		numeroDeHenry="555555";
+		numeroDeSergio="666666";
+		llamada3.setNumeroLlamante(numeroDeHenry);
+		llamada3.setNumeroLlamado(numeroDeSergio);
+		
+		List<Llamada> llamadas = new ArrayList<Llamada>();
+		llamadas.add(llamada1);
+		llamadas.add(llamada1);
+		llamadas.add(llamada1);
+		
+		archivo.guardarLlamada(llamada1);
+		archivo.guardarLlamada(llamada2);
+		archivo.guardarLlamada(llamada3);
+		
+		
+	}
+	
+	public static void guardarUnaLlamada() {
+		
+		Archivo archivo = new Archivo();
+		Llamada llamada1 = new Llamada();
+		String numeroDeHenry="1111111";
+		String numeroDeSergio="2222222";
 		llamada1.setNumeroLlamante(numeroDeHenry);
 		llamada1.setNumeroLlamado(numeroDeSergio);
 		
-		ficherosBinarios.guardarLlamada(llamada1);
-		ficherosBinarios.cargarLlamada();
+		archivo.guardarLlamada(llamada1);
+		
+	}
+	
+	public static void pruebaDeCargarLlamadas() {
+		Archivo archivo = new Archivo();
+		archivo.imprimirLlamadas();
 	}
 	
 	
 	
 	public static void main(String[] args) {
 		
-			pruebasDeGuardaryCargarLlamadas();
+			pruebaDeGuardadVariasLlamadas();
+			pruebaDeCargarLlamadas();
 			System.out.println("TERMINO");
 			
 	}
